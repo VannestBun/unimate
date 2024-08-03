de02648865340adbda1723d4472bb34637839ae2
saririvaii <snurfauziyya@student.unimelb.edu.au>
2024-08-03 18:40:34 +1000

    Added email entry for SSO

--- my-project/src/components/Login.jsx
+++ my-project/src/components/Login.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import unimateLogo from '../assets/Unimate.png';
-import ssoImage from '../assets/ssoLoginImagery.png'; // Replace with your actual image path
+import ssoImage from '../assets/ssoLoginImagery.png';
+import mailIcon from '../assets/iconMail.png'
 
 export default function Login() {
   return (
@@ -20,17 +21,22 @@ export default function Login() {
               Start Your <span className="text-purpleMain">Unforgettable</span> <br />
               First Year with UniMate.
             </h1>
-            <p className="text-lg">
-              Connect with peers, discover exciting events,<br />and embrace all that campus life has to offer. <br />
+            <p className="text-lg tracking-tight">
+              Connect with peers, discover exciting events & <br/>embrace all that campus life has to offer. <br />
               All in one site.
             </p>
 
-            <h2 className="text-xl font-bold tracking-tight mt-8">
+            <h2 className="text-xl font-bold tracking-tight mt-6">
               Log In with SSO
             </h2>
 
             {/* SSO Login Section */}
-            <div className="flex items-center bg-gray-100 rounded-full p-2">
+            <div className="flex items-center bg-gray-100 rounded-full p-2 h-12">
+                <img
+                    src={mailIcon}
+                    alt="Enter Email"
+                    className='h-6 w-6 ml-3'
+                />
                 <input
                     type="email"
                     className="flex-grow bg-transparent text-gray-600 placeholder-gray-900 ml-3 focus:outline-none"
@@ -43,7 +49,7 @@ export default function Login() {
                 </Link>
             </div>
 
-            <div className=" text-sm text-gray-500 hover:text-gray-700 underline mb-24">
+            <div className=" text-sm text-gray-500 hover:text-gray-700 underline mb-28">
               I don’t know my student email
             </div>
           </div>
